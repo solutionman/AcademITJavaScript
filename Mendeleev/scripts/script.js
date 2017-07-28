15cfb0880a1179b8dff520045e3b04b677127a5a
lom <diskedge@yandex.ru>
2017-07-28 10:35:32 +0700

    finally get even cells

--- Mendeleev/scripts/script.js
+++ Mendeleev/scripts/script.js
@@ -1,5 +1,4 @@
 
-
 console.log('script works');
 
 var tableToCount = document.getElementById('tableToCount');
@@ -9,10 +8,23 @@ var rowCount = document.getElementById('tableToCount').getElementsByTagName('tr'
 console.log('amount of rows: ' + rowCount);    // correct
 
 var arrayForRows = document.getElementById('tableToCount').getElementsByTagName('tr');
-console.log(arrayForRows[0]);
-console.log(arrayForRows[1]);
-console.log(arrayForRows[2]);
-
+//console.log(arrayForRows[0]);
+//console.log(arrayForRows[1]);
+//console.log(arrayForRows[2]);
+    
+for (var i = 0; i < arrayForRows.length; ++i){
+    if(i % 2 == 0){
+        console.log(i);
+        //console.log(arrayForRows[i]);
+        var arrayForColumns = arrayForRows[i].getElementsByTagName('th');
+        //console.log(arrayForColumns[2]);
+        for(var j = 0; j < arrayForColumns.length; ++j){
+            if(j % 2 == 0){
+                 console.log(arrayForColumns[j]);
+            }
+        }
+    }
+}
 
 
 
