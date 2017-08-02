fb41b38afc94c4e8cde20bcca1dbee9a59618a60
lom <diskedge@yandex.ru>
2017-08-02 10:30:57 +0700

    data from inputs collected

--- registration/script.js
+++ registration/script.js
@@ -1,10 +1,29 @@
 
-console.log('script works');
+//console.log('script works');
 
 
 function checkcollect(){
-    var name = document.getElementById('name');
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    var country = document.getElementById('country').value;
+    var subscribe = document.getElementById('subscribe').checked;
+    var educationbeginner = document.getElementById('educationbeginner').checked;
+    var school9 = document.getElementById('school9').checked;
+    var school11 = document.getElementById('school11').checked;
+    var schoolmiddle = document.getElementById('schoolmiddle').checked;
+    var schoolhigh = document.getElementById('schoolhigh').checked;
+    var about = document.getElementById('about').value;
+
     console.log(name);
+    console.log(surname);
+    console.log(country);
+    console.log(subscribe);
+    console.log(educationbeginner);
+    console.log(school9);
+    console.log(school11);
+    console.log(schoolmiddle);
+    console.log(schoolhigh);
+    console.log(about);
     //console.log('button works');
 }
 
